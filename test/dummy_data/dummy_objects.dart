bb50ab2740c633c42d0492f857700c7ea6a9fbc8
IzumiShaka-desu <shakaaji29@gmail.com>
2021-10-10 14:16:54 +0700

    provide dummy data for testing

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -57,3 +57,26 @@ final testMovieMap = {
   'posterPath': 'posterPath',
   'title': 'title',
 };
+final testMovieCache = MovieTable(
+  id: 557,
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  title: 'Spider-Man',
+);
+
+final testMovieCacheMap = {
+  'id': 557,
+  'overview':
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  'posterPath': '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  'title': 'Spider-Man',
+};
+
+final testMovieFromCache = Movie.watchlist(
+  id: 557,
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  title: 'Spider-Man',
+);
