b515000f432c2dec88d6f76553063f171090019c
IzumiShaka-desu <shakaaji29@gmail.com>
2021-10-10 14:22:00 +0700

    add NetworkInfo and register to mock

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,3 +1,4 @@
+import 'package:ditonton/common/network_info.dart';
 import 'package:ditonton/data/datasources/db/database_helper.dart';
 import 'package:ditonton/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
@@ -10,6 +11,7 @@ import 'package:http/http.dart' as http;
   MovieRemoteDataSource,
   MovieLocalDataSource,
   DatabaseHelper,
+  NetworkInfo,
 ], customMocks: [
   MockSpec<http.Client>(as: #MockHttpClient)
 ])
