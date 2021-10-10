64e257604db992c75c89e1aa60d3bc14e70e20fd
IzumiShaka-desu <shakaaji29@gmail.com>
2021-10-10 14:24:21 +0700

    add some methods to handle caches

--- lib/data/datasources/movie_local_data_source.dart
+++ lib/data/datasources/movie_local_data_source.dart
@@ -7,6 +7,8 @@ abstract class MovieLocalDataSource {
   Future<String> removeWatchlist(MovieTable movie);
   Future<MovieTable?> getMovieById(int id);
   Future<List<MovieTable>> getWatchlistMovies();
+  Future<void> cacheNowPlayingMovies(List<MovieTable> movies);
+  Future<List<MovieTable>> getCachedNowPlayingMovies();
 }
 
 class MovieLocalDataSourceImpl implements MovieLocalDataSource {
@@ -48,5 +50,21 @@ class MovieLocalDataSourceImpl implements MovieLocalDataSource {
   Future<List<MovieTable>> getWatchlistMovies() async {
     final result = await databaseHelper.getWatchlistMovies();
     return result.map((data) => MovieTable.fromMap(data)).toList();
+  }
+
+  @override
+  Future<void> cacheNowPlayingMovies(List<MovieTable> movies) async {
+    await databaseHelper.clearCache('now playing');
+    await databaseHelper.insertCacheTransaction(movies, 'now playing');
+  }
+
+  @override
+  Future<List<MovieTable>> getCachedNowPlayingMovies() async {
+    final result = await databaseHelper.getCacheMovies('now playing');
+    if (result.length > 0) {
+      return result.map((data) => MovieTable.fromMap(data)).toList();
+    } else {
+      throw CacheException("Can't get the data :(");
+    }
   }
 }
