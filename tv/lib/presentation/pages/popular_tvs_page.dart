979d12bda09de30494a9af427b325646392e573b
IzumiShaka-desu <shakaaji29@gmail.com>
2021-11-20 00:17:45 +0700

    migrate to bloc

--- tv/lib/presentation/pages/popular_tvs_page.dart
+++ tv/lib/presentation/pages/popular_tvs_page.dart
@@ -1,7 +1,6 @@
-import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tv/presentation/provider/popular_tvs_notifier.dart';
+import 'package:tv/presentation/bloc/cubit/popular_tvs/popular_tvs_cubit.dart';
 import 'package:tv/presentation/widgets/tv_card_list.dart';
 
 class PopularTvsPage extends StatefulWidget {
@@ -18,8 +17,7 @@ class _PopularTvsPageState extends State<PopularTvsPage> {
   void initState() {
     super.initState();
     Future.microtask(
-      () => Provider.of<PopularTvsNotifier>(context, listen: false)
-          .fetchPopularTvs(),
+      () => context.read<PopularTvsCubit>().loadPopularTvs(),
     );
   }
 
@@ -27,33 +25,41 @@ class _PopularTvsPageState extends State<PopularTvsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Popular Tvs'),
+        title: const Text('Popular Tvs'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularTvsNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.Loading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (data.state == RequestState.Loaded) {
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  final tv = data.tvs[index];
-                  return TvCard(tv);
-                },
-                itemCount: data.tvs.length,
-              );
-            } else {
-              return Center(
-                key: Key('error_message'),
-                child: Text(data.message),
-              );
-            }
-          },
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: _BodyWidget(),
       ),
+    );
+  }
+}
+
+class _BodyWidget extends StatelessWidget {
+  const _BodyWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<PopularTvsCubit>().state;
+    if (state is ErrorPopularTvsState) {
+      return Center(
+        key: const Key('error_message'),
+        child: Text(state.message),
+      );
+    }
+    if (state is LoadedPopularTvsState) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          final tv = state.tvs[index];
+          return TvCard(tv);
+        },
+        itemCount: state.tvs.length,
+      );
+    }
+    return const Center(
+      child: CircularProgressIndicator(),
     );
   }
 }
