af7e9642311e8acb95da13ac3daaf7114046ce0e
IzumiShaka-desu <shakaaji29@gmail.com>
2021-11-20 14:55:36 +0700

    refactor improve writing

--- about/lib/about_page.dart
+++ about/lib/about_page.dart
@@ -28,7 +28,7 @@ class AboutPage extends StatelessWidget {
                 child: Container(
                   padding: const EdgeInsets.all(32.0),
                   color: kMikadoYellow,
-                  child: Text(
+                  child: const Text(
                     'Ditonton merupakan sebuah aplikasi katalog film yang dikembangkan oleh Dicoding Indonesia sebagai contoh proyek aplikasi untuk kelas Menjadi Flutter Developer Expert.',
                     style: TextStyle(color: Colors.black87, fontSize: 16),
                     textAlign: TextAlign.justify,
@@ -40,7 +40,7 @@ class AboutPage extends StatelessWidget {
           SafeArea(
             child: IconButton(
               onPressed: () => Navigator.pop(context),
-              icon: Icon(Icons.arrow_back),
+              icon: const Icon(Icons.arrow_back),
             ),
           )
         ],
