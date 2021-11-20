278cbb4fa58ea30f1fecde020b05248f0a6329e1
IzumiShaka-desu <shakaaji29@gmail.com>
2021-11-20 17:10:57 +0700

    fix test failed when load json

--- movie/test/sdfdfs.dart
+++ movie/test/sdfdfs.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-String readJson(String name) {
+main() {
+  var name = 'dummy/now_playing.json';
   var dir = Directory.current.path.split('test');
-  if (dir.length < 2) {
-    dir[0] += "/tv";
-  }
   // if (dir.endsWith('/test')) {
   //   dir = dir.replaceAll('/test', '');
   // }
-  return File('${dir[0]}/test/$name').readAsStringSync();
+  var path = '${dir[0]}test/$name';
+  print(path);
+  File(path).readAsStringSync();
 }
